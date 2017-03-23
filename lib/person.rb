d5d8eb5b4cb802703d42e24db536532c183cc3b9
Giovanni Flores <geflores5@gmail.com>
2017-03-23 04:22:21 +0000

    final code

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,56 @@
-require "person/version"
+require 'person/version'
 
 module Person
-  # Your code goes here...
+  class Person
+    # add you method implementations here
+
+    def initialize(name)
+      @name = name
+      @prev = [name]
+      @count = 0
+    end
+
+    def to_s
+      @prev[@count]
+    end
+
+    def titlelize
+      @prev << @name.capitalize
+      @count += 1
+      self
+    end
+
+    def upcase
+      @prev << @name.upcase
+      @count += 1
+      self
+    end
+
+    def downcase
+      @prev << @name.downcase
+      @count += 1
+      self
+    end
+
+    def reverse
+      @prev << @name.reverse.to_s
+      @count += 1
+      self
+    end
+
+    def hyphenize
+      @prev << @name.split("").join("-")
+      @count += 1
+      self
+    end
+
+    def undo
+      if @count > 0
+        @prev.pop
+        @count = @count - 1
+      end
+      self
+    end
+
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,65 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Person do
-  it "has a version number" do
-    expect(Person::VERSION).not_to be nil
+  it '#new' do
+    person = Person::Person.new('giovanni')
+
+    expect(person).to_not be_nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it '#to_s' do
+    person = Person::Person.new('giovanni')
+
+    result = person.to_s
+
+    expect(result).to eq('giovanni')
+  end
+
+  it '#titlelize' do
+    person = Person::Person.new('giovanni')
+
+    person.titlelize
+
+    expect(person.to_s).to eq('Giovanni')
+  end
+
+  it '#upcase' do
+    person = Person::Person.new('giovanni')
+
+    person.upcase
+
+    expect(person.to_s).to eq('GIOVANNI')
+  end
+
+  it '#downcase' do
+    person = Person::Person.new('giovanni')
+
+    person.downcase
+
+    expect(person.to_s).to eq('giovanni')
+  end
+
+  it '#reverse' do
+    person = Person::Person.new('giovanni')
+
+    person.reverse
+
+    expect(person.to_s).to eq('innavoig')
+  end
+
+  it '#hyphenize' do
+    person = Person::Person.new('giovanni')
+
+    person.hyphenize
+
+    expect(person.to_s).to eq('g-i-o-v-a-n-n-i')
+  end
+
+  it '#undo' do
+    person = Person::Person.new('giovanni')
+
+    person.titlelize.upcase.reverse.downcase.undo.undo.undo.undo
+
+    expect(person.to_s).to eq('giovanni')
   end
 end
